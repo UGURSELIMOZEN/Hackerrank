e2f3e95721f226a32db8a5c2aa383ceb9ebbb86d
Uğur Selim Özen <ugur_ozen58@hotmail.com>
2021-07-12 10:21:12 +0300

    Create Introduction to Sets.py

---
+++ Python/Introduction to Sets.py
@@ -0,0 +1,15 @@
+
+
+
+def average(array):
+    my_set = set(array)
+    avg = sum(my_set) / len(my_set)
+    return avg
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
+    
+    
+    
