c94c8860af3127059e971b6270f7da4aa7e8aff6
Uğur Selim Özen <ugur_ozen58@hotmail.com>
2021-07-13 13:53:13 +0300

    Update Weather Observation Station 19.sql

--- SQL/Weather Observation Station 19.sql
+++ SQL/Weather Observation Station 19.sql
@@ -1,4 +1,5 @@
-
+--- Normally round function executes for the 4 digits precision but if it does not work for any reason you should use FORMAT(ROUND( , 2) , 'F2') 
+--- Here 2 is variable of number that you want to get precision digit. It can be   FORMAT(ROUND( , 3) , 'F3') , FORMAT(ROUND( , 5) , 'F5') and etc.
 
 
 
