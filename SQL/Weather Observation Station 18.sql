beb78e0a51c770c30474ae06caf5e968494e1d01
Uğur Selim Özen <ugur_ozen58@hotmail.com>
2021-07-13 13:55:24 +0300

    Update Weather Observation Station 18.sql

--- SQL/Weather Observation Station 18.sql
+++ SQL/Weather Observation Station 18.sql
@@ -3,6 +3,8 @@
 --- Here 2 is variable of number that you want to get precision digit. It can be   FORMAT(ROUND( , 3) , 'F3') , FORMAT(ROUND( , 5) , 'F5') and etc.
  
  
+--- This is the formula for Manhattan distance   : https://xlinux.nist.gov/dads/HTML/manhattanDistance.html 
+
  
 SELECT FORMAT( ROUND (ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)) , 4) , 'F4')
 FROM STATION ;
