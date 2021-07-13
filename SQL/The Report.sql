dc4dec380385d8011e7a4c4f29af05e3dc31283a
Uğur Selim Özen <ugur_ozen58@hotmail.com>
2021-07-13 14:34:24 +0300

    Update The Report.sql

--- SQL/The Report.sql
+++ SQL/The Report.sql
@@ -1,14 +1,7 @@
 
 
 
-SELECT
-  CASE
-    WHEN GRADES.grade < 8 THEN NULL
-    ELSE STUDENTS.name
-  END,
-  GRADES.grade,
-  STUDENTS.marks
-FROM students 
+SELECT CASE WHEN GRADES.grade < 8 THEN NULL ELSE STUDENTS.name END , GRADES.grade , STUDENTS.marks FROM students 
 INNER JOIN GRADES ON STUDENTS.marks >= GRADES.min_mark AND STUDENTS.marks <= GRADES.max_mark
 ORDER BY GRADES.grade DESC, STUDENTS.name ;
 
