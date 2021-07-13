c40751779181b73e5c110dd2dced61788310ebb0
Uğur Selim Özen <ugur_ozen58@hotmail.com>
2021-07-13 13:54:10 +0300

    Update Weather Observation Station 19.sql

--- SQL/Weather Observation Station 19.sql
+++ SQL/Weather Observation Station 19.sql
@@ -1,6 +1,7 @@
 --- Normally round function executes for the 4 digits precision but if it does not work for any reason you should use FORMAT(ROUND( , 2) , 'F2') 
 --- Here 2 is variable of number that you want to get precision digit. It can be   FORMAT(ROUND( , 3) , 'F3') , FORMAT(ROUND( , 5) , 'F5') and etc.
 
+---- this is the formula for euclidean distance  : https://en.wikipedia.org/wiki/Euclidean_distance
 
 
 SELECT FORMAT (ROUND (SQRT(SQUARE(MAX(LAT_N)-MIN(LAT_N)) + SQUARE(MAX(LONG_W)-MIN(LONG_W))),4) , 'F4') 
